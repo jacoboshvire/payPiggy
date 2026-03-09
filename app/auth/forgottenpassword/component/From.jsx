aed321ae10aa5462d751167a848e5021f160add0
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-09 13:44:10 +0000

    auto commit @Mon Mar  9 13:44:10 GMT 2026

--- app/auth/forgottenpassword/component/From.jsx
+++ app/auth/forgottenpassword/component/From.jsx
@@ -67,7 +67,7 @@ export default function From() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
                 className="seePassword"
-                onClick={changeType}
+                onClick={changeTypeFun}
               >
                 <path
                   fillRule="evenodd"
