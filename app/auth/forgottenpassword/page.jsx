13f88f93b79745c33ae11d8182c799157fe54eb6
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-09 13:28:20 +0000

    auto commit @Mon Mar  9 13:28:20 GMT 2026

--- app/auth/forgottenpassword/page.jsx
+++ app/auth/forgottenpassword/page.jsx
@@ -1,19 +1,18 @@
-
 "use client";
 
 import { useState, useEffect } from "react";
 import Back from "../../component/Back";
 import Detail from "./component/Detail";
 import From from "./component/From";
-import "./component/style.css"
+import "./component/style.css";
 
 export default function page() {
   return (
-    <div className='forgotPassword'>
+    <div className="forgotPassword">
       <div className="forgotPassword_container">
-      <Back />
-      <From />
-      <Detail />
+        <Back />
+        <From />
+        <Detail />
       </div>
     </div>
   );
