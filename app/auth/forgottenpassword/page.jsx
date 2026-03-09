854a386186ca7573c1479367ed69644f568c321f
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-09 13:04:54 +0000

    auto commit @Mon Mar  9 13:04:54 GMT 2026

--- app/auth/forgottenpassword/page.jsx
+++ app/auth/forgottenpassword/page.jsx
@@ -4,11 +4,14 @@
 import { useState, useEffect } from "react";
 import Back from "../../component/Back";
 import Detail from "./component/Detail";
+import From from "./component/From";
 
 export default function page() {
   return (
     <div className='forgotPassword'>
       <Back />
+      <From />
+      <Detail />
     </div>
   );
 }
