b4c92c919ae68d431927f5d416f31b2e35aa9a36
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-10 23:08:45 +0000

    auto commit @Tue Mar 10 23:08:45 GMT 2026

--- app/dashboard/@Home/component/WelcomeMsg.jsx
+++ app/dashboard/@Home/component/WelcomeMsg.jsx
@@ -6,5 +6,16 @@ import { useRouter } from "next/navigation";
 
 export default function WelcomeMsg() {
   const router = useRouter();
-  return <div>WelcomeMsg</div>;
+  return (
+    <div className='home_welcomeMsg'>
+      <Image
+        src={
+          "https://res.cloudinary.com/dr0yyqvj6/image/upload/v1767871145/nxn2zpymtgyxybpjhqqg.jpg"
+        }
+        alt='profile'
+        height={50}
+        width={50}
+      />
+    </div>
+  );
 }
