0f8e8657449e9781efcb110562ed43460c670def
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-10 14:08:24 +0000

    auto commit @Tue Mar 10 14:08:24 GMT 2026

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Nav from "./component/Nav";
+import "./style.css";
 
 export default function layout({ Account, Home, Payment, Wallet }) {
   return (
