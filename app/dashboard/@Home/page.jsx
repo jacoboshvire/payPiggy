a204f29f47dd230514ef97e49362c2160bfd2394
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-10 14:28:30 +0000

    auto commit @Tue Mar 10 14:28:30 GMT 2026

--- app/dashboard/@Home/page.jsx
+++ app/dashboard/@Home/page.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 export default function page() {
-  const pathname = usePathname;
-  const searchParams = useSearchParams;
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
   return (
     <>
       {pathname === "/dashboard" && searchParams.toString() === "" && (
