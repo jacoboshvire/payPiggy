fc5f650bc93d7d720189a0c8f1f5c175d8ef90c7
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-10 13:46:10 +0000

    auto commit @Tue Mar 10 13:46:10 GMT 2026

--- app/dashboard/component/Nav.jsx
+++ app/dashboard/component/Nav.jsx
@@ -1,5 +1,11 @@
 "use client";
 import { useState, useEffect } from "react";
+import {
+  useRouter,
+  useParams,
+  usePathname,
+  useSearchParams,
+} from "next/navigation";
 import "./style.css";
 
 export default function Nav() {
