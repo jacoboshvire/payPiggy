563848794863729a89b9629a518e8341e0e2db45
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-10 23:04:10 +0000

    auto commit @Tue Mar 10 23:04:10 GMT 2026

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+/** @format */
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
