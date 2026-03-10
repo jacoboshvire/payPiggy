344e35fd83b6ce5433e571939206c0e98f62be8b
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-10 23:07:14 +0000

    auto commit @Tue Mar 10 23:07:14 GMT 2026

--- app/dashboard/@Home/component/WelcomeMsg.jsx
+++ app/dashboard/@Home/component/WelcomeMsg.jsx
@@ -2,7 +2,9 @@
 "use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function WelcomeMsg() {
+  const router = useRouter();
   return <div>WelcomeMsg</div>;
 }
