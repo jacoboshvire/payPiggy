a1b249b330b8667870e67396f330c3933d72ea27
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-11 15:50:27 +0000

    auto commit @Wed Mar 11 15:50:27 GMT 2026

--- app/dashboard/@Account/component/AccountLink.jsx
+++ app/dashboard/@Account/component/AccountLink.jsx
@@ -4,5 +4,14 @@ import React from "react";
 import Image from "next/image";
 
 export default function AccountLink() {
-  return <div>AccountLink</div>;
+  return (
+    <div className='Account_Link'>
+      <Image
+        src='https://res.cloudinary.com/dr0yyqvj6/image/upload/v1767871145/nxn2zpymtgyxybpjhqqg.jpg'
+        alt='profile'
+        height={100}
+        width={100}
+      />
+    </div>
+  );
 }
