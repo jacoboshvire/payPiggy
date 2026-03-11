380e370c1ea771c6c8d86fe5499b61f16521de3f
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-11 10:34:31 +0000

    auto commit @Wed Mar 11 10:34:31 GMT 2026

--- app/dashboard/@Home/page.jsx
+++ app/dashboard/@Home/page.jsx
@@ -5,9 +5,9 @@ import React from "react";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import "./style.css";
 import Card from "../component/Card";
-import PaymentHistory from "./component/PaymentHistory";
+import PaymentHistory from "../component/PaymentHistory";
 import WelcomeMsg from "./component/WelcomeMsg";
-import Transaction from "./component/Transaction";
+import Transaction from "../component/Transaction";
 
 export default function page() {
   const pathname = usePathname();
