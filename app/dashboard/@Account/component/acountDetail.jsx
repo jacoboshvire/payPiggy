55473c04e0ffc99d7c81ea6337e5581bce13d4d1
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-11 15:07:53 +0000

    auto commit @Wed Mar 11 15:07:53 GMT 2026

--- app/dashboard/@Account/component/acountDetail.jsx
+++ app/dashboard/@Account/component/acountDetail.jsx
@@ -7,7 +7,7 @@ export default function AcountDetail() {
   return (
     <div>
       {LinkArray.map((link, index) => {
-        return <p key={index}>{link.linkname}</p>;
+        return <p key={index}>{link.linkName}</p>;
       })}
     </div>
   );
