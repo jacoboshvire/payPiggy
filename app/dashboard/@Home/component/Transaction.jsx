b31064e6bc2cc1f60a7c2bd4536914765f329ba6
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-11 10:05:08 +0000

    auto commit @Wed Mar 11 10:05:08 GMT 2026

--- app/dashboard/@Home/component/Transaction.jsx
+++ app/dashboard/@Home/component/Transaction.jsx
@@ -7,8 +7,21 @@ export default function Transaction() {
   return (
     <div className='Home_transaction'>
       <div className='Home_transaction_btu'>
-        <div className='Home_transaction_icon'></div>
-        <div className='Home_transacton_linkName'></div>
+        <div className='Home_transaction_icon'>
+          <svg
+            width='30'
+            height='30'
+            viewBox='0 0 30 30'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M16 14H23.5V16H16V23.5H14V16H6.5V14H14V6.5H16V14Z'
+              fill='black'
+            />
+          </svg>
+        </div>
+        <div className='Home_transacton_linkName'>Add</div>
       </div>
     </div>
   );
