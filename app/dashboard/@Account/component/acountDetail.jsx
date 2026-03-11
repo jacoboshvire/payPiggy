8ca887a6af3cb8b68c9276792bf68f4a4bbf59c9
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-11 15:12:59 +0000

    auto commit @Wed Mar 11 15:12:59 GMT 2026

--- app/dashboard/@Account/component/acountDetail.jsx
+++ app/dashboard/@Account/component/acountDetail.jsx
@@ -2,13 +2,16 @@
 
 import React from "react";
 import LinkArray from "./LinkArray";
+import Link from "next/link";
 
 export default function AccountDetail() {
   return (
-    <div className="Account_Detail">
+    <div className='Account_Detail'>
       {LinkArray.map((link, index) => {
         return (
-
+          <Link href={link.link}>
+            <div className='svg'>{link.linksvg}</div>
+          </Link>
         );
       })}
     </div>
