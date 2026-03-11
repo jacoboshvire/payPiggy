e5f7c2fae05c638bb4ca953bb153aa7b06b6d62b
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-11 10:04:37 +0000

    auto commit @Wed Mar 11 10:04:37 GMT 2026

--- app/dashboard/@Home/component/Transaction.jsx
+++ app/dashboard/@Home/component/Transaction.jsx
@@ -4,5 +4,12 @@ import { useState, useEffect } from "react";
 import "../style.css";
 
 export default function Transaction() {
-  return <div>Transaction</div>;
+  return (
+    <div className='Home_transaction'>
+      <div className='Home_transaction_btu'>
+        <div className='Home_transaction_icon'></div>
+        <div className='Home_transacton_linkName'></div>
+      </div>
+    </div>
+  );
 }
