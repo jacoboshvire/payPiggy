91e0009243dbdfef76ef2b5c0e7a6f45ed97032d
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-11 15:02:13 +0000

    auto commit @Wed Mar 11 15:02:13 GMT 2026

--- app/dashboard/@Account/component/acountDetail.jsx
+++ app/dashboard/@Account/component/acountDetail.jsx
@@ -4,5 +4,9 @@ import React from "react";
 import LinkArray from "./LinkArray";
 
 export default function AcountDetail() {
-  return <div>AcountDetail</div>;
+  return (
+    <div>
+      <LinkArray />
+    </div>
+  );
 }
