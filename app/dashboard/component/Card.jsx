9bc92841b78171421a88c83c645802d20ceba18c
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-12 13:40:56 +0000

    auto commit @Thu Mar 12 13:40:56 GMT 2026

--- app/dashboard/component/Card.jsx
+++ app/dashboard/component/Card.jsx
@@ -14,7 +14,7 @@ export default function vault() {
   return (
     <m.div
       className='card'
-      initial={{ opacity: 1, scale: 0.8 }}
+      initial={{ opacity: 0, scale: 0.8 }}
       animate={{ opacity: 1, scale: 1, transition: { duration: 1 } }}
       viewport={true}
     >
