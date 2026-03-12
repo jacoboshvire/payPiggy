27b03d4268ec39baee1434ac6ffaa251a2ced10f
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-12 13:21:18 +0000

    auto commit @Thu Mar 12 13:21:18 GMT 2026

--- app/dashboard/@Wallet/component/Main.jsx
+++ app/dashboard/@Wallet/component/Main.jsx
@@ -5,7 +5,7 @@ import Vault from "../../component/vault";
 import Card from "../../component/Card";
 
 export default function Main() {
-  const [openCard, setOpenCard] = useState(false);
+  const [openCard, setOpenCard] = useState(true);
 
   const toggleFun = () => {
     setOpenCard((openCard) => !openCard);
