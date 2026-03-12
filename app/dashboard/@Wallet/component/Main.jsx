b609f01ee2e800eed0e24700947115c8b66860a4
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-12 13:25:26 +0000

    auto commit @Thu Mar 12 13:25:26 GMT 2026

--- app/dashboard/@Wallet/component/Main.jsx
+++ app/dashboard/@Wallet/component/Main.jsx
@@ -56,6 +56,7 @@ export default function Main() {
             viewBox='0 0 24 24'
             fill='none'
             xmlns='http://www.w3.org/2000/svg'
+            className={openVault ? "wallet_icon" : ""}
           >
             <path
               fillRule='evenodd'
