683e228e07e73a104e71ed5a932f9e57e2782698
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-12 12:03:40 +0000

    auto commit @Thu Mar 12 12:03:40 GMT 2026

--- app/dashboard/@Wallet/component/Main.jsx
+++ app/dashboard/@Wallet/component/Main.jsx
@@ -5,6 +5,7 @@ import Vault from "../../component/vault";
 import Card from "../../component/Card";
 
 export default function Main() {
+  const [openCard, setOpenCard] = useState(false);
   return (
     <div className='wallet_main'>
       <Card />
