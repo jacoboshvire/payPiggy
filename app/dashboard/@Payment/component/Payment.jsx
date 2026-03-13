9723df661215d4bc47a0db7954ca278cce635d82
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-13 08:30:03 +0000

    auto commit @Fri Mar 13 08:30:03 GMT 2026

--- app/dashboard/@Payment/component/Payment.jsx
+++ app/dashboard/@Payment/component/Payment.jsx
@@ -5,7 +5,13 @@ import { useEffect, useState } from "react";
 export default function Payment() {
   return (
     <div className='payment_form'>
-      <form></form>
+      <form>
+        <div className='payment_form_input'>
+          <label htmlFor=''>
+            <p>Account name</p>
+          </label>
+        </div>
+      </form>
     </div>
   );
 }
