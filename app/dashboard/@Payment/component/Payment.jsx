3c60adf9e3f80b1809f5a6523d511aa1754d0e19
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-13 08:36:41 +0000

    auto commit @Fri Mar 13 08:36:41 GMT 2026

--- app/dashboard/@Payment/component/Payment.jsx
+++ app/dashboard/@Payment/component/Payment.jsx
@@ -6,17 +6,19 @@ export default function Payment() {
   return (
     <div className='payment_form'>
       <form>
-        <div className='payment_form_input'>
-          <label htmlFor='account_no_input'>
-            <p>Account number</p>
-          </label>
-          <input type='text' name='accountNumber' id='account_no_input' />
-        </div>
-        <div className='payment_form_input'>
-          <label htmlFor='sort_code'>
-            <p>Sort code</p>
-          </label>
-          <input type='text' name='sortCode' id='sort_code' />
+        <div className='payment_input_column'>
+          <div className='payment_form_input'>
+            <label htmlFor='account_no_input'>
+              <p>Account number</p>
+            </label>
+            <input type='text' name='accountNumber' id='account_no_input' />
+          </div>
+          <div className='payment_form_input'>
+            <label htmlFor='sort_code'>
+              <p>Sort code</p>
+            </label>
+            <input type='text' name='sortCode' id='sort_code' />
+          </div>
         </div>
       </form>
     </div>
