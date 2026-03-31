a0218ef3c36aff0d33f48dcccd2b5eded891a0c8
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-03-31 08:04:59 +0100

    auto commit @Tue Mar 31 08:04:59 BST 2026

--- app/auth/otp-options/component/details.js
+++ app/auth/otp-options/component/details.js
@@ -7,7 +7,7 @@ export default function details() {
       <Logo />
 
       <h2 className='otp_title'>Verify your identity</h2>
-      <p>
+      <p className='otp_desc'>
         Choose how you’d like to receive your one-time verification code (OTP).
       </p>
     </>
