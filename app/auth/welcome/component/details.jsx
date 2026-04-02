3c47b0e9e27f0d9171191c6d15579bb6ed048cb2
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-02 13:41:29 +0100

    auto commit @Thu Apr  2 13:41:29 BST 2026

--- app/auth/welcome/component/details.jsx
+++ app/auth/welcome/component/details.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Back from "../../../component/Back";
 import Logo from "../../../component/Logo";
+import "./details.css";
 
 export default function details() {
   return (
