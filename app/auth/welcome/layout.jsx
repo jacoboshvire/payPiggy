c765b49bb74095eea2bf0b11840e19cf440065f2
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-02 13:32:41 +0100

    auto commit @Thu Apr  2 13:32:41 BST 2026

--- app/auth/welcome/layout.jsx
+++ app/auth/welcome/layout.jsx
@@ -1,10 +1,12 @@
 /** @format */
 
 import React from "react";
+import Details from "./component/details";
 
 export default function layout({ Account, ChooseImage, PhoneNumber }) {
   return (
     <div>
+        <Details
       {Account}
       {ChooseImage}
       {PhoneNumber}
