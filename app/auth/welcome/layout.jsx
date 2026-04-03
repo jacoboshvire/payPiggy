d0478f0927ee41de5566f82e38c272d33472241a
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-03 17:11:12 +0100

    auto commit @Fri Apr  3 17:11:12 BST 2026

--- app/auth/welcome/layout.jsx
+++ app/auth/welcome/layout.jsx
@@ -7,11 +7,14 @@ import { usePathname } from "next/navigation";
 
 export default function layout({ Account, ChooseImage }) {
   const pathname = usePathname();
+
   return (
-    <div className='welcome'>
-      <Details />
-      {Account && Account}
-      {ChooseImage && ChooseImage}
-    </div>
+    <>
+      <div className='welcome'>
+        <Details />
+        {Account && Account}
+        {ChooseImage && ChooseImage}
+      </div>
+    </>
   );
 }
