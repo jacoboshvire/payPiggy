921a6a47175b052df08584a65a496e19a2b5c641
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-03 17:48:04 +0100

    auto commit @Fri Apr  3 17:48:04 BST 2026

--- app/dashboard/@Account/component/acountDetail.jsx
+++ app/dashboard/@Account/component/acountDetail.jsx
@@ -1,11 +1,13 @@
 /** @format */
-
+"use client";
 import React from "react";
 import LinkArray from "./LinkArray";
 import Link from "next/link";
 import "../style.css";
+import { useRouter } from "next/navigation";
 
 export default function AccountDetail() {
+  const router = useRouter();
   const handleLogout = () => {
     localStorage.removeItem("token");
     router.push("/login");
