5856f8d7c6f16146ad84a1dec972f5fbc9e1085a
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-03 17:00:08 +0100

    auto commit @Fri Apr  3 17:00:08 BST 2026

--- app/auth/welcome/layout.jsx
+++ app/auth/welcome/layout.jsx
@@ -1,10 +1,13 @@
 /** @format */
-
+"use client";
 import React from "react";
 import Details from "./component/details";
 import "./style.css";
+import { usePathname } from "next/navigation";
 
 export default function layout({ Account, ChooseImage }) {
+  const pathname = usePathname();
+
   return (
     <div className='welcome'>
       <Details />
