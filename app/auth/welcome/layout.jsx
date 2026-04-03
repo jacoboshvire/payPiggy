b7a4a19ed85269444aa59529fb4466202c6fcfed
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-03 17:12:43 +0100

    auto commit @Fri Apr  3 17:12:43 BST 2026

--- app/auth/welcome/layout.jsx
+++ app/auth/welcome/layout.jsx
@@ -3,18 +3,21 @@
 import React from "react";
 import Details from "./component/details";
 import "./style.css";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function layout({ Account, ChooseImage }) {
   const pathname = usePathname();
+  const router = useRouter();
+
+  if (pathname === "/auth/welcome") {
+    router.push("/auth/welcome?Account=true");
+  }
 
   return (
-    <>
-      <div className='welcome'>
-        <Details />
-        {Account && Account}
-        {ChooseImage && ChooseImage}
-      </div>
-    </>
+    <div className='welcome'>
+      <Details />
+      {Account && Account}
+      {ChooseImage && ChooseImage}
+    </div>
   );
 }
