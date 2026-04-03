9196baffc65c36688ef8f49a794427f1c1aa2237
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-03 17:23:51 +0100

    auto commit @Fri Apr  3 17:23:51 BST 2026

--- app/auth/welcome/@ChooseImage/page.jsx
+++ app/auth/welcome/@ChooseImage/page.jsx
@@ -1,7 +1,16 @@
 /** @format */
-
+"use client";
 import React from "react";
+import { useSearchParams, usePathname } from "next/navigation";
 
 export default function page() {
-  return <div>choose image</div>;
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  <>
+  {
+    if (pathname.includes("/auth/welcome") && searchParams.get("ChooseImage") === "true") {
+      return <div>choose image</div>;
+    }
+  }
+  </>
 }
