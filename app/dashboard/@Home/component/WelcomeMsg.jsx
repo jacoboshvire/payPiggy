2ae7c95d34a33e7c0ee3a3fb3e89f603b98d255d
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-04 11:00:59 +0100

    auto commit @Sat Apr  4 11:00:59 BST 2026

--- app/dashboard/@Home/component/WelcomeMsg.jsx
+++ app/dashboard/@Home/component/WelcomeMsg.jsx
@@ -40,7 +40,7 @@ export default function WelcomeMsg() {
         <Image
           src={
             avatar ||
-            "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+            "https://res.cloudinary.com/dhyjebn3i/image/upload/q_auto/f_auto/v1774959019/cld-sample.jpg"
           }
           alt='profile'
           height={50}
