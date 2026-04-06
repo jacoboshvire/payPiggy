1a87ac5d300c24b37accda222818e1e3c19e368d
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-06 10:40:07 +0100

    auto commit @Mon Apr  6 10:40:07 BST 2026

--- app/dashboard/@Add/page.jsx
+++ app/dashboard/@Add/page.jsx
@@ -1,6 +1,11 @@
 /** @format */
-
+"use client";
 import { useEffect, useState } from "react";
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
+import { api } from "@/lib/api";
+import Cookies from "js-cookie";
+import { saveToken } from "@/lib/auth";
+import "../style.css";
 
 export default function Add() {
   return <div>page</div>;
