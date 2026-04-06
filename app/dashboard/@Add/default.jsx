34f31cf15f71161cf4a5190fb8cbe4f2a451a6c0
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-06 10:39:35 +0100

    auto commit @Mon Apr  6 10:39:35 BST 2026

--- app/dashboard/@Add/default.jsx
+++ app/dashboard/@Add/default.jsx
@@ -1,13 +1,5 @@
 /** @format */
 
-"use client";
-import { useState, useEffect } from "react";
-import "../style.css";
-import { useRouter, useSearchParams, usePathname } from "next/navigation";
-
 export default function Defaults() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
   return <div>default</div>;
 }
--- app/dashboard/@Add/page.jsx
+++ app/dashboard/@Add/page.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from "react";
+import { useEffect, useState } from "react";
 
 export default function Add() {
   return <div>page</div>;
