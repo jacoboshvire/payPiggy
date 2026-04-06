52b5f9c080ef307c298b79dc52949f2a708715c5
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-06 12:21:53 +0100

    auto commit @Mon Apr  6 12:21:53 BST 2026

--- app/dashboard/component/Card.jsx
+++ app/dashboard/component/Card.jsx
@@ -1,6 +1,6 @@
 /** @format */
 "use client";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Card from "../../../public/Group.svg";
 import { api } from "@/lib/api";
