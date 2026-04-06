3eafe4160cf314a8511fcd8596942314c31f0824
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-06 11:52:53 +0100

    auto commit @Mon Apr  6 11:52:53 BST 2026

--- app/dashboard/@Add/page.jsx
+++ app/dashboard/@Add/page.jsx
@@ -64,6 +64,32 @@ export default function Add() {
           <div className='Home_add'>
             <div className='title'>
               <h1>Add Money</h1>
+
+              <div className="cancel_btu">
+                <svg
+                  width='24'
+                  height='24'
+                  viewBox='0 0 24 24'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                  onClick={() => router.push("/dashboard")}
+                >
+                  <line
+                    x1='18.364'
+                    y1='5.63604'
+                    x2='5.63599'
+                    y2='18.364'
+                    stroke='#606060'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                  />
+                  <line
+                    x1='5.63599'
+                    y1='5.63604'
+                    x2='18.364'
+                    y2='18.364'
+                    stroke='#606060'            strokeWidth='2'                   strokeLinecap='round'                 />  
+              </div>
             </div>
             <div className='form'>
               <form onSubmit={handleSubmit}>
