b59d189bca395d9f876f6c6e439d43a0c128f21a
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-06 10:48:47 +0100

    auto commit @Mon Apr  6 10:48:47 BST 2026

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -5,11 +5,12 @@ import React from "react";
 import Nav from "./component/Nav";
 import "./style.css";
 
-export default function layout({ Account, Home, Payment, Wallet }) {
+export default function layout({ Account, Home, Payment, Wallet, Add }) {
   return (
     <div className='bashboard'>
       <div className='bashboard_container'>
         <Nav />
+        {Add && Add}
         {Home && Home}
         {Payment && Payment}
         {Wallet && Wallet}
