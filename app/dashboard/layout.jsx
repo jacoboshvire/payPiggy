daf24dc21ea50047a2e945dfdcfe7b3974fec2a4
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-06 10:54:56 +0100

    auto commit @Mon Apr  6 10:54:56 BST 2026

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,11 +1,19 @@
 /** @format */
 
 "use client";
-import React from "react";
+import { useEffect } from "react";
 import Nav from "./component/Nav";
+import { usePathname, useRouter } from "next/navigation";
 import "./style.css";
 
 export default function layout({ Account, Home, Payment, Wallet, Add }) {
+  const pathname = usePathname();
+  const router = useRouter();
+  useEffect(() => {
+    if (pathname === "/dashboard") {
+      router.push("/dashboard?home=true");
+    }
+  }, [pathname, router]);
   return (
     <div className='bashboard'>
       <div className='bashboard_container'>
