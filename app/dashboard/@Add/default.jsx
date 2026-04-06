90cec49d4f44eec307aa3c4cdff445284d09b05f
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-06 10:38:33 +0100

    auto commit @Mon Apr  6 10:38:33 BST 2026

--- app/dashboard/@Add/default.jsx
+++ app/dashboard/@Add/default.jsx
@@ -2,6 +2,8 @@
 
 "use client";
 import { useState, useEffect } from "react";
+import "../style.css";
+import { useRouter } from "next/navigation";
 
 export default function Defaults() {
   return <div>default</div>;
