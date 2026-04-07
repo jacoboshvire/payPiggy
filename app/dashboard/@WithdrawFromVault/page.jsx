d104ee60a6366bbd4fa00deb0675e4410fce1a4e
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-07 11:21:46 +0100

    auto commit @Tue Apr  7 11:21:46 BST 2026

--- app/dashboard/@WithdrawFromVault/page.jsx
+++ app/dashboard/@WithdrawFromVault/page.jsx
@@ -120,6 +120,33 @@ export default function WithdrawFromVault({ onSuccess }) {
                 <>
                   <div className='title'>
                     <h1>Vault Withdrawal</h1>
+                    <div className='cancel_btu'>
+                      <svg
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
+                        onClick={() => router.push("/dashboard?home=true")}
+                      >
+                        <line
+                          x1='18.364'
+                          y1='5.63604'
+                          x2='5.63599'
+                          y2='18.364'
+                          strokeWidth='2'
+                          strokeLinecap='round'
+                        />
+                        <line
+                          x1='5.63599'
+                          y1='5.63604'
+                          x2='18.364'
+                          y2='18.364'
+                          strokeWidth='2'
+                          strokeLinecap='round'
+                        />
+                      </svg>
+                    </div>
                   </div>
                   <p>Balance: £{Number(vault.balance).toFixed(2)}</p>
                   <p>
