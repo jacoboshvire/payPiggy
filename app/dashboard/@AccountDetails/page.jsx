94c37416d2c4b9e55b9880c8429ef12696d44f3b
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-08 21:30:58 +0100

    auto commit @Wed Apr  8 21:30:58 BST 2026

--- app/dashboard/@AccountDetails/page.jsx
+++ app/dashboard/@AccountDetails/page.jsx
@@ -1,7 +1,23 @@
 /** @format */
 
-import React from "react";
+import { useEffect, useState } from "react";
 
 export default function account() {
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const fetchAccount = async () => {
+      try {
+        const accountId = localStorage.getItem("accountId");
+        const data = await api.get(`/api/account/${accountId}`);
+        console.log(data);
+        setAccount(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchAccount();
+  }, []);
   return <div>page</div>;
 }
