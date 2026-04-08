9d0ab5d2c8cee4aaa4031db09e5abe85e81a69a3
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-09 00:45:15 +0100

    auto commit @Thu Apr  9 00:45:15 BST 2026

--- app/dashboard/@AccountDetails/page.jsx
+++ app/dashboard/@AccountDetails/page.jsx
@@ -33,71 +33,66 @@ export default function Account() {
     fetchAccount();
   }, []);
 
-  if (
-    pathname.includes("dashboard") &&
-    searchParams.get("account_details") === "true"
-  ) {
-    return null;
-  }
-
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
   if (!account) return <p>Account not found</p>;
 
   return (
-    <div className='accountDetail'>
-      <div className='accountDetail_avatar'>
-        <Image
-          src={
-            user?.avatar ||
-            "https://res.cloudinary.com/dhyjebn3i/image/upload/q_auto/f_auto/v1774959207/Avatar_ql2szp.png"
-          }
-          alt='profile'
-          height={100}
-          width={100}
-        />
+    <>
+      <div className='accountDetail'>
+        <div className='accountDetail_avatar'>
+          <Image
+            src={
+              user?.avatar ||
+              "https://res.cloudinary.com/dhyjebn3i/image/upload/q_auto/f_auto/v1774959207/Avatar_ql2szp.png"
+            }
+            alt='profile'
+            height={100}
+            width={100}
+          />
+        </div>
+
+        <div className='accountDetail_info'>
+          <div className='accountDetail_item'>
+            <p className='accountDetail_label'>Full Name</p>
+            <p className='accountDetail_value'>
+              {account.first_name} {account.last_name}
+            </p>
+          </div>
+
+          <div className='accountDetail_item'>
+            <p className='accountDetail_label'>Account Number</p>
+            <p className='accountDetail_value'>{account.account_number}</p>
+          </div>
+
+          <div className='accountDetail_item'>
+            <p className='accountDetail_label'>Sort Code</p>
+            <p className='accountDetail_value'>{account.sort_code}</p>
+          </div>
+
+          <div className='accountDetail_item'>
+            <p className='accountDetail_label'>Balance</p>
+            <p className='accountDetail_value'>
+              £{Number(account.balance).toFixed(2)}
+            </p>
+          </div>
+
+          <div className='accountDetail_item'>
+            <p className='accountDetail_label'>Account Type</p>
+            <p className='accountDetail_value'>{account.account_type}</p>
+          </div>
+
+          <div className='accountDetail_item'>
+            <p className='accountDetail_label'>Email</p>
+            <p className='accountDetail_value'>{user?.email}</p>
+          </div>
+
+          <div className='accountDetail_item'>
+            <p className='accountDetail_label'>Phone</p>
+            <p className='accountDetail_value'>{user?.phone}</p>
+          </div>
+        </div>
       </div>
-
-      <div className='accountDetail_info'>
-        <div className='accountDetail_item'>
-          <p className='accountDetail_label'>Full Name</p>
-          <p className='accountDetail_value'>
-            {account.first_name} {account.last_name}
-          </p>
-        </div>
-
-        <div className='accountDetail_item'>
-          <p className='accountDetail_label'>Account Number</p>
-          <p className='accountDetail_value'>{account.account_number}</p>
-        </div>
-
-        <div className='accountDetail_item'>
-          <p className='accountDetail_label'>Sort Code</p>
-          <p className='accountDetail_value'>{account.sort_code}</p>
-        </div>
-
-        <div className='accountDetail_item'>
-          <p className='accountDetail_label'>Balance</p>
-          <p className='accountDetail_value'>
-            £{Number(account.balance).toFixed(2)}
-          </p>
-        </div>
-
-        <div className='accountDetail_item'>
-          <p className='accountDetail_label'>Account Type</p>
-          <p className='accountDetail_value'>{account.account_type}</p>
-        </div>
-
-        <div className='accountDetail_item'>
-          <p className='accountDetail_label'>Email</p>
-          <p className='accountDetail_value'>{user?.email}</p>
-        </div>
-
-        <div className='accountDetail_item'>
-          <p className='accountDetail_label'>Phone</p>
-          <p className='accountDetail_value'>{user?.phone}</p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
