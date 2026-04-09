58687235ddc525070abfd63ccd299a1cfe7d2917
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-09 12:41:16 +0100

    auto commit @Thu Apr  9 12:41:16 BST 2026

--- app/dashboard/@AccountDetails/page.jsx
+++ app/dashboard/@AccountDetails/page.jsx
@@ -63,7 +63,7 @@ export default function Account() {
                     viewBox='0 0 24 24'
                     fill='none'
                     xmlns='http://www.w3.org/2000/svg'
-                    onClick={() => router.push("/dashboard?account=true")}
+                    onClick={() => router.back()}
                   >
                     <line
                       x1='18.364'
