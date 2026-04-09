bb18f7e38b196afab5f112659ff28dac882ffaa1
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-09 12:39:39 +0100

    auto commit @Thu Apr  9 12:39:39 BST 2026

--- app/dashboard/@AccountDetails/page.jsx
+++ app/dashboard/@AccountDetails/page.jsx
@@ -51,6 +51,37 @@ export default function Account() {
       {pathname.includes("/dashboard") &&
         searchParams.get("account_details") === "true" && (
           <div className='accountDetail_bg'>
+            <div className='title'>
+              <h1>Add Money</h1>
+
+              <div className='cancel_btu'>
+                <svg
+                  width='24'
+                  height='24'
+                  viewBox='0 0 24 24'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                  onClick={() => router.push("/dashboard?account=true")}
+                >
+                  <line
+                    x1='18.364'
+                    y1='5.63604'
+                    x2='5.63599'
+                    y2='18.364'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                  />
+                  <line
+                    x1='5.63599'
+                    y1='5.63604'
+                    x2='18.364'
+                    y2='18.364'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                  />
+                </svg>
+              </div>
+            </div>
             <div className='accountDetail'>
               <div className='accountDetail_info'>
                 <div className='account_names'>
