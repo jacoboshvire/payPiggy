ae0a64c9bac41be0b8550c46adb4b3faca3064b3
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-13 11:18:32 +0100

    auto commit @Mon Apr 13 11:18:32 BST 2026

--- app/auth/welcome/@Kyc/page.jsx
+++ app/auth/welcome/@Kyc/page.jsx
@@ -66,7 +66,7 @@ export default function UpdateProfile() {
 
   return (
     <>
-      {pathname === "/auth/welcome" && Kyc && (
+      {pathname.includes("/auth/welcome") && Kyc && (
         <div className='updateName'>
           <form onSubmit={handleSubmit}>
             <div className='inputField'>
