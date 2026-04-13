fb445eb13672cb9d61c5973ddf880e3984a495b7
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-13 10:14:04 +0100

    auto commit @Mon Apr 13 10:14:04 BST 2026

--- app/dashboard/@Add/page.jsx
+++ app/dashboard/@Add/page.jsx
@@ -78,7 +78,7 @@ export default function Add() {
                   viewBox='0 0 24 24'
                   fill='none'
                   xmlns='http://www.w3.org/2000/svg'
-                  onClick={() => router.push("/dashboard?home=true")}
+                  onClick={() => router.back()}
                 >
                   <line
                     x1='18.364'
