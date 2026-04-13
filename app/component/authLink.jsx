403dc8e291ab3eaba449f750a91ff6cec307f9ff
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-13 11:42:33 +0100

    auto commit @Mon Apr 13 11:42:33 BST 2026

--- app/component/authLink.jsx
+++ app/component/authLink.jsx
@@ -1,7 +1,26 @@
 /** @format */
 
 import React from "react";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function authLink() {
-  return <div>authLink</div>;
+  const pathname = usePathname();
+  const router = useRouter();
+
+  return (
+    <div>
+      {pathname === "/auth/login" && (
+        <p>
+          Don't have an account?{" "}
+          <span onClick={() => router.push("/auth/signup")}>Sign Up</span>
+        </p>
+      )}
+      {pathname === "/auth/signup" && (
+        <p>
+          Already have an account?{" "}
+          <span onClick={() => router.push("/auth/login")}>Log In</span>
+        </p>
+      )}
+    </div>
+  );
 }
