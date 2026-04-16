65bec657c147c73c002d5ccde4f4ef4d7fd61c16
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-16 10:00:39 +0100

    auto commit @Thu Apr 16 10:00:39 BST 2026

--- app/auth/welcome/component/details.jsx
+++ app/auth/welcome/component/details.jsx
@@ -4,20 +4,22 @@ import React from "react";
 import Back from "../../../component/Back";
 import Logo from "../../../component/Logo";
 import "./details.css";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export default function Details() {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const isChooseImage = searchParams.get("ChooseImage") === "true";
+
   return (
     <div className='welcome_details'>
       <Back />
       <Logo />
       <div className='welcome_title'>
-        {pathname.includes("/auth/welcome") &&
-        pathname.includes("ChooseImage=true") ? (
+        {pathname.includes("/auth/welcome") && isChooseImage ? (
           <h1>Choose your profile picture</h1>
         ) : (
-          <h1>We’d like to get to know you better</h1>
+          <h1>We'd like to get to know you better</h1>
         )}
         <p>please provide a few more details to create your account.</p>
       </div>
