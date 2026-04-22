d50242a4133196c90095d189ddacdb2ce02395e0
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-22 09:57:30 +0100

    auto commit @Wed Apr 22 09:57:30 BST 2026

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@
 
 const BASE_URL =
   process.env.NEXT_PUBLIC_API_URL ||
-  "http://localhost:8000"; /* Fallback to localhost if env variable is not set */
+  "http://localhost:8080"; /* Fallback to localhost if env variable is not set */
 
 const getToken = () => {
   if (typeof window !== "undefined") {
