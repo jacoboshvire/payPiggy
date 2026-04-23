71443fe06060807ffd9dd3a3a8e69ad283a79bc2
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-23 11:33:45 +0100

    auto commit @Thu Apr 23 11:33:45 BST 2026

--- app/auth/otp-options/page.js
+++ app/auth/otp-options/page.js
@@ -1,7 +1,7 @@
 /** @format */
 
 "use client";
-import OtpOptions from "./component/otpoptions";
+import OtpOptions from "./components/otpoptions";
 import Back from "../../component/Back";
 
 export default function page() {
