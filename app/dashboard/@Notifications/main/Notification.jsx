3157d84eaafb11efa8a3deef30318fa816f36b7c
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-28 21:42:28 +0100

    auto commit @Tue Apr 28 21:42:28 BST 2026

--- app/dashboard/@Notifications/main/Notification.jsx
+++ app/dashboard/@Notifications/main/Notification.jsx
@@ -3,7 +3,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { api } from "@/lib/api";
-import "./notifications.css";
+import "../notification.css";
 
 export default function Notifications() {
   const [notifications, setNotifications] = useState([]);
