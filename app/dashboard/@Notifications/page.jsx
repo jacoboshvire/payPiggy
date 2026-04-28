48b9c0a99d39b049fb155e7b075c3ea4f691739d
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-28 21:39:22 +0100

    auto commit @Tue Apr 28 21:39:22 BST 2026

--- app/dashboard/@Notifications/page.jsx
+++ app/dashboard/@Notifications/page.jsx
@@ -6,5 +6,12 @@ import { useSearchParams, usePathname } from "next/navigation";
 export default function Page() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  return <div>page</div>;
+  const notifications = searchParams.get("notifications");
+  return (
+    <>
+      {pathname.includes("dashboard") && notifications === "true" && (
+        <div className='notification-banner'>You have new notifications!</div>
+      )}
+    </>
+  );
 }
