e347c3a5dc0db5f9f9e7b6f64e2d29ef5d7d7e02
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-28 21:38:21 +0100

    auto commit @Tue Apr 28 21:38:21 BST 2026

--- app/dashboard/@Notifications/page.jsx
+++ app/dashboard/@Notifications/page.jsx
@@ -5,5 +5,6 @@ import { useSearchParams, usePathname } from "next/navigation";
 
 export default function Page() {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
   return <div>page</div>;
 }
