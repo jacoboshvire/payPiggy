39dd779cbfb70b2ba45a3bd62caa7c126d7b4974
Jacob Oshevire <ejirojacob2000@gmail.com>
2026-04-28 21:37:49 +0100

    auto commit @Tue Apr 28 21:37:49 BST 2026

--- app/dashboard/@Notifications/page.jsx
+++ app/dashboard/@Notifications/page.jsx
@@ -1,7 +1,9 @@
 /** @format */
 
-import React from "react";
+"use client";
+import { useSearchParams, usePathname } from "next/navigation";
 
 export default function Page() {
+  const searchParams = useSearchParams();
   return <div>page</div>;
 }
